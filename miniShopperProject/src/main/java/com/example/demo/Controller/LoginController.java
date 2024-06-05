d3e532cee40fff89925e92a57543e21dd457a798
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-05 21:21:12 +0530

    modified some methods

--- miniShopperProject/src/main/java/com/example/demo/Controller/LoginController.java
+++ miniShopperProject/src/main/java/com/example/demo/Controller/LoginController.java
@@ -14,7 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,8 +28,11 @@ import com.example.demo.Entity.User;
 import com.example.demo.Repository.LoginDataRepository;
 import com.example.demo.Repository.UserRepository;
 import com.example.demo.Response.LoginResponse;
+import com.example.demo.Service.LoginDataService;
+import com.example.demo.Service.UserService;
 import com.example.demo.Service.impl.UserServiceImpl;
 import com.example.demo.dtos.LoginDto;
+import com.example.demo.dtos.UserDto;
 import com.example.demo.exception.LoginException;
 
 
@@ -37,25 +42,18 @@ import com.example.demo.exception.LoginException;
 @Controller
 @RequestMapping("/users/")
 public class LoginController {
-	// just checking git bash working
-    @Autowired
-	UserRepository ur;
+    
+    @Autowired 
+    LoginDataService loginDataService;
+
     
     @Autowired
-    LoginDataRepository loginRepo;
+    UserService userService;
     
     @Autowired
-    UserServiceImpl userServiceImpl;
+	private ModelMapper modelMapper;
     
-    
-    
-    @GetMapping("loginUser")
-    public LoginDto getuser() {
-    	LoginDto ld=new LoginDto();
-    	ld.setPassword("12edwedD");
-    	ld.setUserId("GopalaKrishnan@gmail.com");
-    	return ld;
-    }
+  
 	 
 	@PostMapping("loginUser")
 	public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginDto l) {
@@ -65,41 +63,41 @@ public class LoginController {
 		LocalTime time=LocalTime.now();
 		
 		LoginResponse lr=new LoginResponse();
-		
+		lr.setStatus("");
 		if(l.getUserId()!=null && l.getPassword()!=null) {
 
-			
-			User loginUser=userServiceImpl.checkUserId(l.getUserId());
+			User loginUser=userService.checkUserId(l.getUserId());
 			if(loginUser==null) {
-				//System.out.println("the objecct is null");
 				
 				LoginData ld=new LoginData(l.getUserId(),"Invalid UserId and password",date,time);
-				loginRepo.save(ld);
+				loginDataService.saveLoginData(ld);
 				lr.setStatus("404");
 				lr.setStatusMessage("NOT_FOUND");
 				lr.setMessage("You have entered a Invalid UserId");
 				
 				return new ResponseEntity<LoginResponse>(lr,HttpStatus.NOT_FOUND);	
-				//return ResponseEntity.status(HttpStatus.OK).body("User Not Found");		
+					
 			}
 			
 			if(loginUser.getPassword().toString().equals(l.getPassword())) {
-				//res=new ResponseEntity<User>(loginUser,HttpStatus.OK);
+			
+                UserDto userDto=modelMapper.map(loginUser, UserDto.class);
+                
+				lr=LoginResponse.builder().user(userDto).build();
+				
 				System.out.println("login success");
 				lr.setStatus("200");
 				lr.setStatusMessage("OK");
 				lr.setMessage("Login Success");
-				
 				LoginData ld=new LoginData(l.getUserId(),"Login Success",date,time);
-				loginRepo.save(ld);
+				loginDataService.saveLoginData(ld);
 
 				return new ResponseEntity<LoginResponse>(lr,HttpStatus.OK);	
 				
-//				return ResponseEntity.status(HttpStatus.OK).body("Login Success");
 			}else {
 				
 				LoginData ld=new LoginData(l.getUserId(),"Incorrect password",date,time);
-				loginRepo.save(ld);
+				loginDataService.saveLoginData(ld);
 				
 				System.out.println("login failed"); 
 				lr.setStatus("401");
@@ -109,7 +107,6 @@ public class LoginController {
 
 				return new ResponseEntity<LoginResponse>(lr,HttpStatus.UNAUTHORIZED);	
 				
-				//return ResponseEntity.status(HttpStatus.OK).body("Login Failed");
 			}
 			
 		}else {
@@ -119,7 +116,6 @@ public class LoginController {
 			return new ResponseEntity<LoginResponse>(lr,HttpStatus.BAD_REQUEST);
 		}
 		
-//		System.out.println("entity response "+res);
 	}
 	
 	@PostMapping("testHere")
@@ -132,15 +128,21 @@ public class LoginController {
 		
 	}
 	
-	
-	
-	
-	
-	@GetMapping("allUsers")
-	public ResponseEntity<List<User>> getAllUsers(){
-		System.out.println("getting into all users");
-		return new ResponseEntity<List<User>>(ur.findAll(),HttpStatus.OK); 
+	@PostMapping("{userId}")
+	public ResponseEntity<UserDto> getUserById(@PathVariable String userId){
+		
+		UserDto ud=userService.fetchUserDetailsById(userId);
+		return new ResponseEntity<UserDto>(ud,HttpStatus.OK);
 	}
+	
+	@PutMapping("{userId}")
+	public ResponseEntity<UserDto> updateUserDetails(@PathVariable String userId, @RequestBody UserDto userDto){
+		
+		UserDto user=userService.updateUser(userId, userDto);
+		return new ResponseEntity<UserDto>(user,HttpStatus.OK);
+		
+	}
+
 	
 
 }
