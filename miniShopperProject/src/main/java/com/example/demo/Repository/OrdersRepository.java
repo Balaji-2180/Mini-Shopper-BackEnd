162d22e7a68cf92c89ca45003d471d16b2378399
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-06 16:14:21 +0530

    added file

---
+++ miniShopperProject/src/main/java/com/example/demo/Repository/OrdersRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.Entity.Orders;
+
+
+
+
+
+public interface OrdersRepository extends JpaRepository<Orders, String>{
+	
+
+}
