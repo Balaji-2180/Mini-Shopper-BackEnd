c07dc2a552a2de6ae966f49ffd4d47908312621a
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-06 16:11:23 +0530

    added file

---
+++ miniShopperProject/src/main/java/com/example/demo/dtos/ProductDto.java
@@ -0,0 +1,29 @@
+package com.example.demo.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductDto {
+	
+	private String productId;
+	
+	private String	productName;
+
+	private String	brand;
+
+    private double	unitPrice;
+
+	private int	stock;
+
+	private String	category;
+
+}
