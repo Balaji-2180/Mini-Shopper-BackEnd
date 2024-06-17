1cc6c385fd6cc1ffcf7c8574d5ca8192cd251d77
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-17 12:00:07 +0530

    modified files

--- miniShopperProject/src/main/java/com/example/demo/Entity/User.java
+++ miniShopperProject/src/main/java/com/example/demo/Entity/User.java
@@ -1,10 +1,16 @@
 package com.example.demo.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,6 +40,8 @@ public class User {
 	@Column(name = "image", nullable = true)
 	private String image;
 	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, mappedBy = "user")
+	private List<Order> orders = new ArrayList<>();
 
 	
 	public User() {
