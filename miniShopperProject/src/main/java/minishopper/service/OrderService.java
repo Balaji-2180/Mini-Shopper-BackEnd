7cd683a1a42bf91a8db7367dda372f789af8ee1c
Balaji-2180 <balaji.a8@cognizant.com>
2024-07-03 15:37:16 +0530

    updated for order status

--- miniShopperProject/src/main/java/minishopper/service/OrderService.java
+++ miniShopperProject/src/main/java/minishopper/service/OrderService.java
@@ -1,28 +1,33 @@
-package com.example.demo.Service;
+package minishopper.service;
 
 import java.util.List;
 
-import com.example.demo.Entity.OrderItem;
-import com.example.demo.dtos.CreateOrderRequest;
-import com.example.demo.dtos.OrderDto;
-import com.example.demo.dtos.OrderItemDto;
-import com.example.demo.dtos.UpdateOrderItem;
+import minishopper.dto.ChangeOrderStatus;
+import minishopper.dto.CreateOrderRequestDto;
+import minishopper.dto.OrderDto;
+import minishopper.dto.OrderItemDto;
+import minishopper.dto.UpdateOrderItemDto;
+import minishopper.entity.OrderItem;
 
 public interface OrderService {
-	OrderDto createOrder(CreateOrderRequest orderRequest);
-	
-    OrderDto createOrderSingleProduct(CreateOrderRequest orderRequest);
-	
+	OrderDto createOrder(CreateOrderRequestDto orderRequest);
+
+	OrderDto createOrderSingleProduct(CreateOrderRequestDto orderRequest);
+
 	List<OrderDto> fetchOrderByUser(String userId);
-	
+
 	OrderDto fetchOrderByOrderId(String orderId);
-	
+
 	OrderItem removeOrderItemByOrderItemId(int orderItemId);
-	
+
 	void updateTotalPrice(OrderItem removePrice);
+
+	OrderItemDto updateOrderItem(UpdateOrderItemDto updateOrderItem);
+
+	OrderDto createOrderByExcelSheet(CreateOrderRequestDto orderRequest);
 	
-	OrderItemDto updateOrderItem(UpdateOrderItem updateOrderItem);
+	List<OrderDto> fetchAllOrders();
 	
-	OrderDto createOrderByExcelSheet(CreateOrderRequest orderRequest);
-	
+	void updateOrderStatus(ChangeOrderStatus changeOrderStatus);
+
 }
