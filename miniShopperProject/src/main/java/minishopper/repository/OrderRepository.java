91cb4b8acf88b5f6786aecdd3c592ae66afa693b
Balaji-2180 <balaji.a8@cognizant.com>
2024-07-03 15:40:02 +0530

    updated for order status

--- miniShopperProject/src/main/java/minishopper/repository/OrderRepository.java
+++ miniShopperProject/src/main/java/minishopper/repository/OrderRepository.java
@@ -1,17 +1,27 @@
-package com.example.demo.Repository;
+package minishopper.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import com.example.demo.Entity.Order;
-import com.example.demo.Entity.User;
+import jakarta.transaction.Transactional;
+import minishopper.entity.Order;
+import minishopper.entity.User;
 
-public interface OrderRepository extends JpaRepository<Order, String>{
+public interface OrderRepository extends JpaRepository<Order, String> {
 
 	Order findOrderByOrderId(String orderId);
-	
+
 	List<Order> findByUser(User user);
 	
-	
+	@Transactional
+	@Modifying
+	@Query("update orders o set o.orderStatus = :orderStatus, o.reason = :reason, o.deliveredDate = :date where o.orderId = :orderId")
+	void updateOrderStatusByOrderId(@Param("orderStatus") String orderStatus, @Param("orderId") String orderId, 
+			                        @Param("reason") String reason, @Param("date") LocalDate date);
+
 }
