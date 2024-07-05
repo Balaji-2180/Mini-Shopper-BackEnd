bd3e5a2b385f08a081c4312cbe1b3344bff3f64a
Balaji-2180 <balaji.a8@cognizant.com>
2024-07-05 13:27:40 +0530

    updated after completing US_06

--- miniShopperProject/src/main/java/minishopper/config/SecurityConfig.java
+++ miniShopperProject/src/main/java/minishopper/config/SecurityConfig.java
@@ -1,17 +1,11 @@
-package com.example.demo.config;
-
+package minishopper.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-
-
-
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,12 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.example.demo.Service.CustomUserDetailsService;
-import com.example.demo.security.JwtAuthenticationFilter;
-
-
-
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
@@ -33,49 +22,60 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-
+import minishopper.security.JwtAuthenticationFilter;
+import minishopper.service.CustomUserDetailsService;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
-	
+
 	@Autowired
 	private CustomUserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private JwtAuthenticationFilter authenticationFilter;
-	
-	
+
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+		daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		return daoAuthenticationProvider;
+	}
+
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		//System.out.println("in SecurityConfig securityFilterChain");
-		http.cors((cors)-> cors.configurationSource(corsFilter())).csrf().disable().authorizeHttpRequests()
-		
-		.requestMatchers("/users/loginUser").permitAll()
-		.requestMatchers("/users/newUser").permitAll()
-		.requestMatchers(HttpMethod.GET).permitAll().anyRequest().authenticated().and()
-
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		// System.out.println("in SecurityConfig securityFilterChain");
+		http.cors((cors) -> cors.configurationSource(corsFilter())).csrf().disable().authorizeHttpRequests()
+		//		.requestMatchers(HttpMethod.POST, "/users/**").permitAll()
+				.requestMatchers("/users/newUser").permitAll().requestMatchers("/users/loginUser").permitAll()
+				.requestMatchers(HttpMethod.GET).permitAll().anyRequest().authenticated().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
-		
-		
+
 		return http.build();
 	}
-	
+
 	@Bean
-	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-			throws Exception {
-	//	System.out.println("in SecurityConfig authenticationManager");
-		
+	public SecurityFilterChain filterChainXss(HttpSecurity http) throws Exception {
+		XXssProtectionHeaderWriter xXssProtectionHeaderWriter = new XXssProtectionHeaderWriter();
+		http.headers(headers -> headers
+				.xssProtection(xss -> xss.headerValue(XXssProtectionHeaderWriter.HeaderValue.ENABLED_MODE_BLOCK))
+				.contentSecurityPolicy(cps -> cps.policyDirectives("script-src 'self'")));
+		return http.build();
+	}
+
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
-	} 
-	
+	}
+
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	
+
 	@Bean
 	CorsConfigurationSource corsFilter() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -97,28 +97,8 @@ public class SecurityConfig {
 		FilterRegistrationBean<CorsFilter> filterRegistrationBean = new FilterRegistrationBean<CorsFilter>(
 				new CorsFilter(source));
 		filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		
+
 		return source;
 	}
-	
-	
-	
-	
-	
-	@Bean
-	DaoAuthenticationProvider authenticationProvider() {
-		
-		//System.out.println("in SecurityConfig authenticationProvider");
-		
-		
-		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-		daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-		
-		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-		
-		return daoAuthenticationProvider;
-	}
-	
-	
 
 }
