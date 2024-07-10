b91b1c26cec907a251994c664f19b213c7ca1a98
Balaji-2180 <balaji.a8@cognizant.com>
2024-07-10 17:46:17 +0530

    updated for validation checking

--- miniShopperProject/src/main/java/minishopper/service/UserService.java
+++ miniShopperProject/src/main/java/minishopper/service/UserService.java
@@ -2,16 +2,17 @@ package minishopper.service;
 
 import java.util.List;
 
+import minishopper.dto.AddressDto;
 import minishopper.dto.UserDto;
 import minishopper.entity.User;
 
 public interface UserService {
 
-	public User saveUser(User user);
+	public User saveUser(UserDto userDto);
 
 	public User checkUserId(String userId);
 
 	public UserDto fetchUserDetailsById(String userId);
 
-	public UserDto updateUser(String userId, UserDto userDto);
+	public UserDto updateUser(String userId, AddressDto address);
 }
