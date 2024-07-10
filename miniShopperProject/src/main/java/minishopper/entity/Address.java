4fd0df4a8c8d1b2c5b2dfcbec164477ad55fcdb9
Balaji-2180 <balaji.a8@cognizant.com>
2024-07-10 17:50:36 +0530

    added file

---
+++ miniShopperProject/src/main/java/minishopper/entity/Address.java
@@ -0,0 +1,55 @@
+package minishopper.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "Address")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int addressId;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	@Column(name = "address", nullable = false)
+	private String address;
+	
+	@Column(name = "street", nullable = false)
+	private String street;
+	
+	@Column(name = "city", nullable = false)
+	private String city;
+	
+	@Column(name = "state", nullable = false)
+	private String state;
+	
+	@Column(name = "pinCode", nullable = false)
+	private String pinCode;
+
+
+	
+
+}
