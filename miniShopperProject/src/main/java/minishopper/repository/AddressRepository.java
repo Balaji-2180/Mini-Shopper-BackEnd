1155e865ea9174810a0f06c3c0590d9c0f92434a
Balaji-2180 <balaji.a8@cognizant.com>
2024-07-12 21:47:57 +0530

    updated file

--- miniShopperProject/src/main/java/minishopper/repository/AddressRepository.java
+++ miniShopperProject/src/main/java/minishopper/repository/AddressRepository.java
@@ -3,11 +3,23 @@ package minishopper.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import jakarta.transaction.Transactional;
 import minishopper.dto.AddressDto;
 import minishopper.entity.Address;
 import minishopper.entity.User;
 
 public interface AddressRepository extends JpaRepository<Address, Integer>{
       List<Address> findByUser(User user);
+      
+  	@Transactional
+  	@Modifying
+  	@Query("update address a set a.addressLine = :addressLine, a.addressType = :addressType, a.city = :city, a.pinCode = :pinCode, a.state = :state, a.street = :street  where a.addressId = :addressId")
+  	void updateAddressById(@Param("addressId") int addressId, @Param("addressLine") String addressLine,@Param("addressType") String addressType,  
+  			                        @Param("city") String city, @Param("pinCode") String pinCode, @Param("state") String state,@Param("street") String street); 
+
+
 }
